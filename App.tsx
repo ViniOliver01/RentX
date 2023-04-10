8eb728958538ff898952804f9131189b9ca56ecb
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2023-04-10 10:53:51 -0300

    init project

---
+++ App.tsx
@@ -0,0 +1,54 @@
+import { NavigationContainer } from "@react-navigation/native";
+import * as SplashScreen from "expo-splash-screen";
+import React, { useCallback, useEffect } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { ThemeProvider } from "styled-components";
+
+import { Home } from "./src/pages/Home/Home";
+import theme from "./src/styles/theme";
+
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
+import { Inter_400Regular, Inter_500Medium, useFonts } from "@expo-google-fonts/inter";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer onReady={onLayoutRootView}>
+          <Home />;
+        </NavigationContainer>
+      </ThemeProvider>
+    </GestureHandlerRootView>
+  );
+}
