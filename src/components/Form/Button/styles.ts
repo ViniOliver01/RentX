edc7bbc83272024a4f4a4f4b9d2acf95239163c3
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2023-04-11 13:48:27 -0300

    created button component

---
+++ src/components/Form/Button/styles.ts
@@ -0,0 +1,15 @@
+import { RectButton } from "react-native-gesture-handler";
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+
+export const Container = styled(RectButton)`
+  background-color: ${({ theme }) => theme.colors.main};
+  padding: 20px;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_500};
+  font-size: ${RFValue(15)}px;
+  color: ${({ theme }) => theme.colors.background_secondary};
+`;
