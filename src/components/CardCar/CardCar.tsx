f15afec6f528f49db0055c2eeeffb985dbac95df
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2023-04-12 09:44:03 -0300

    added button props

--- src/components/CardCar/CardCar.tsx
+++ src/components/CardCar/CardCar.tsx
@@ -11,19 +11,29 @@ import {
   PriceDetails,
 } from "./styles";
 
+import { RectButtonProps } from "react-native-gesture-handler";
 import DefaultCar from "../../assets/defaultCar.svg";
 import Electric from "../../assets/electric.svg";
 import Gas from "../../assets/gas.svg";
 
-interface CardCarProps {
+interface CardCarProps extends RectButtonProps {
   manufactor: string;
   model: string;
   fuelType: string;
   diary: number;
   carImage: string;
+  onPress: () => void;
 }
 
-export function CardCar({ manufactor, model, fuelType, diary, carImage }: CardCarProps) {
+export function CardCar({
+  manufactor,
+  model,
+  fuelType,
+  diary,
+  carImage,
+  onPress,
+  ...rest
+}: CardCarProps) {
   const price = new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
@@ -31,7 +41,7 @@ export function CardCar({ manufactor, model, fuelType, diary, carImage }: CardCa
   }).format(diary);
 
   return (
-    <Container>
+    <Container onPress={onPress} {...rest}>
       <CarDetails>
         <Manufactor>{manufactor.toUpperCase()}</Manufactor>
         <Model>{model}</Model>
