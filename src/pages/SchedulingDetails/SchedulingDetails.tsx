d4b29770d8644ac67edc5eec87b3b10e37fca2e3
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2023-04-12 08:56:34 -0300

    added Scheduling Details

---
+++ src/pages/SchedulingDetails/SchedulingDetails.tsx
@@ -0,0 +1,140 @@
+import React from "react";
+import { StatusBar } from "react-native";
+import { useTheme } from "styled-components";
+import { IconButton } from "../../components/Form/IconButton/IconButton";
+import {
+  ButtonArea,
+  CarDailyRate,
+  CarName,
+  Container,
+  Daily,
+  DailyTitle,
+  DateArea,
+  DateBox,
+  Details,
+  Header,
+  IconWrapper,
+  Manufacturer,
+  Model,
+  SchedulingContent,
+  SelectedDate,
+  SelectedDateDate,
+  SelectedDateTitle,
+  TotalValue,
+  ValueBox,
+  ValueDetails,
+  ValueDetailsExplain,
+  ValueDetailsTitle,
+} from "./styles";
+
+import { Button } from "../../components/Form/Button/Button";
+import { ImageSlider } from "../../components/ImageSlider/ImageSlider";
+import { TechnicalFeatures } from "../../components/TechnicalFeatures/TechnicalFeatures";
+
+import { Feather } from "@expo/vector-icons";
+import BackButton from "../../assets/Back.svg";
+import CalendarIcon from "../../assets/Calendar.svg";
+
+export function SchedulingDetails() {
+  const theme = useTheme();
+
+  const car = {
+    id: "1",
+    manufacturer: "Toyota",
+    model: "Corolla",
+    fuelType: "Gas",
+    daily: 580,
+    image: "https://www.toyotaaruba.com/readBlob.do?id=14860&width=190&height=",
+    description:
+      "Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.",
+  };
+  const startDate = "18/06/2021";
+  const endDate = "20/06/2021";
+
+  const days = 3;
+
+  const carDaily = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+    minimumFractionDigits: 0,
+  }).format(car.daily);
+
+  const totalValue = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+    minimumFractionDigits: 0,
+  }).format(car.daily * 3);
+
+  return (
+    <Container>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor={theme.colors.background_secondary}
+      />
+
+      <Header>
+        <IconButton
+          icon={<BackButton width={32} height={32} />}
+          onPress={() => console.log("Back")}
+        />
+      </Header>
+
+      <ImageSlider
+        imagesUrl={["https://www.toyotaaruba.com/readBlob.do?id=14860&width=190&height="]}
+      />
+
+      <Details>
+        <CarName>
+          <Manufacturer>{car.manufacturer.toUpperCase()}</Manufacturer>
+          <Model>{car.model}</Model>
+        </CarName>
+
+        <CarDailyRate>
+          <DailyTitle>AO DIA</DailyTitle>
+          <Daily>{carDaily}</Daily>
+        </CarDailyRate>
+      </Details>
+      <TechnicalFeatures />
+
+      <SchedulingContent>
+        <DateBox>
+          <IconWrapper>
+            <CalendarIcon width={24} height={24} />
+          </IconWrapper>
+
+          <DateArea>
+            <SelectedDate>
+              <SelectedDateTitle>DE</SelectedDateTitle>
+              <SelectedDateDate>{startDate}</SelectedDateDate>
+            </SelectedDate>
+
+            <Feather name="chevron-right" color={theme.colors.text_detail} size={24} />
+
+            <SelectedDate>
+              <SelectedDateTitle>ATÉ</SelectedDateTitle>
+              <SelectedDateDate>{endDate}</SelectedDateDate>
+            </SelectedDate>
+          </DateArea>
+        </DateBox>
+
+        <ValueBox>
+          <ValueDetails>
+            <ValueDetailsTitle>TOTAL</ValueDetailsTitle>
+
+            <ValueDetailsExplain>
+              {carDaily} x {days} diárias
+            </ValueDetailsExplain>
+          </ValueDetails>
+          <TotalValue>{totalValue}</TotalValue>
+        </ValueBox>
+      </SchedulingContent>
+
+      <ButtonArea>
+        <Button
+          title="Escolher período do aluguel"
+          onPress={() => console.log("Press")}
+        />
+      </ButtonArea>
+    </Container>
+  );
+}
---
+++ src/pages/SchedulingDetails/styles.ts
@@ -0,0 +1,116 @@
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  background-color: ${({ theme }) => theme.colors.background_secondary};
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  position: absolute;
+  margin: 30px 24px;
+`;
+
+export const Details = styled.View`
+  padding: 0 24px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const CarName = styled.View``;
+
+export const CarDailyRate = styled.View``;
+
+export const Manufacturer = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  font-size: ${RFValue(10)}px;
+  color: ${({ theme }) => theme.colors.text_detail};
+`;
+
+export const Model = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  font-size: ${RFValue(25)}px;
+  color: ${({ theme }) => theme.colors.title};
+`;
+
+export const DailyTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  font-size: ${RFValue(10)}px;
+  color: ${({ theme }) => theme.colors.text_detail};
+`;
+
+export const Daily = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  font-size: ${RFValue(25)}px;
+  color: ${({ theme }) => theme.colors.main};
+`;
+
+export const SchedulingContent = styled.View`
+  flex: 1;
+  padding: 0 24px;
+  justify-content: space-evenly;
+`;
+
+export const DateBox = styled.View`
+  flex-direction: row;
+  align-items: center;
+  border-bottom-width: 1px;
+  border-bottom-color: ${({ theme }) => theme.colors.line};
+  padding-right: 16px;
+`;
+
+export const IconWrapper = styled.View`
+  background-color: ${({ theme }) => theme.colors.main};
+  padding: 12px;
+  margin-right: 24px;
+`;
+
+export const DateArea = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  flex: 1;
+`;
+
+export const SelectedDate = styled.View``;
+
+export const SelectedDateTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  font-size: ${RFValue(10)}px;
+  color: ${({ theme }) => theme.colors.text_detail};
+`;
+
+export const SelectedDateDate = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_500};
+  font-size: ${RFValue(15)}px;
+  color: ${({ theme }) => theme.colors.title};
+`;
+
+export const ValueBox = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+export const ValueDetails = styled.View``;
+
+export const ValueDetailsTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  font-size: ${RFValue(10)}px;
+  color: ${({ theme }) => theme.colors.text_detail};
+`;
+export const ValueDetailsExplain = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_500};
+  font-size: ${RFValue(15)}px;
+  color: ${({ theme }) => theme.colors.title};
+`;
+
+export const TotalValue = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  font-size: ${RFValue(24)}px;
+  color: ${({ theme }) => theme.colors.success};
+`;
+
+export const ButtonArea = styled.View`
+  background-color: ${({ theme }) => theme.colors.background_primary};
+  padding: 24px;
+`;
