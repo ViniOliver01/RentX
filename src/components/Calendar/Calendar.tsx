6715452260199d9fdc59c5d07e646d27c37ebfff
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2023-04-12 15:52:33 -0300

    updated calendar configs

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,81 +1,43 @@
 import { Feather } from "@expo/vector-icons";
-import React, { useState } from "react";
+import React from "react";
 import {
+  CalendarProps,
   Calendar as CustomCalendar,
-  DateData,
   LocaleConfig,
 } from "react-native-calendars";
 import { useTheme } from "styled-components";
+import { ptBR } from "./localeConfig";
 
 LocaleConfig.defaultLocale = "pt-br";
-LocaleConfig.locales["pt-br"] = {
-  monthNames: [
-    "Janeiro",
-    "Fevereiro",
-    "Março",
-    "Abril",
-    "Maio",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro",
-  ],
-  monthNamesShort: [
-    "Jan",
-    "Fev",
-    "Mar",
-    "Abr",
-    "Mai",
-    "Jun",
-    "Jul",
-    "Ago",
-    "Set",
-    "Out",
-    "Nov",
-    "Dez",
-  ],
-  dayNames: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
-  dayNamesShort: ["DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SÁB"],
-  today: "Hoje",
-};
+LocaleConfig.locales["pt-br"] = ptBR;
 
-export function Calendar() {
+export interface MarkedDateProps {
+  [date: string]: {
+    color: string;
+    textColor: string;
+    disabled?: boolean;
+    disabledTouchEvent?: boolean;
+  };
+}
+
+export interface DayProps {
+  dateString: string;
+  day: number;
+  month: number;
+  timestamp: number;
+  year: number;
+}
+
+export function Calendar({ markedDates, onDayPress }: CalendarProps) {
   const theme = useTheme();
-  const [startDay, setStartDay] = useState<DateData>();
-  const [endDay, setEndDay] = useState<DateData>();
-
-  function handleSelectDay(day: DateData) {
-    if (!startDay) {
-      setStartDay(day);
-      console.log(day.dateString);
-    }
-    if (startDay && !endDay) {
-      setEndDay(day);
-    }
-  }
 
   return (
     <CustomCalendar
-      onDayPress={handleSelectDay}
+      onDayPress={onDayPress}
       firstDay={1}
       minDate={String(new Date())}
       markingType={"period"}
-      markedDates={{
-        "2023-04-13": {
-          color: theme.colors.main,
-          textColor: theme.colors.background_secondary,
-        },
-        "2023-04-14": { color: theme.colors.main_light, textColor: theme.colors.main },
-        "2023-04-15": { color: theme.colors.main_light, textColor: theme.colors.main },
-        "2023-04-16": { color: theme.colors.main_light, textColor: theme.colors.main },
-        "2023-04-17": {
-          color: theme.colors.main,
-          textColor: theme.colors.background_secondary,
-        },
-      }}
+      markedDates={markedDates}
       renderArrow={(direction) => (
         <Feather
           name={direction === "left" ? "chevron-left" : "chevron-right"}
