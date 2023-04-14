d246d0343cf0de5e85679970a48b5100a7c357db
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2023-04-14 10:41:18 -0300

    updated formatted Data

--- src/utils/getPlatformDate.ts
+++ src/utils/getPlatformDate.ts
@@ -2,9 +2,9 @@ import { addDays } from "date-fns";
 import { Platform } from "react-native";
 
 export function getPlatformDate(date: Date) {
-  if (Platform.OS === "ios") {
+  if (Platform.OS === "ios" || (Platform.OS === "android" && Platform.Version > 31)) {
     return addDays(date, 1);
   } else {
-    return addDays(date, 1);
+    return date;
   }
 }
